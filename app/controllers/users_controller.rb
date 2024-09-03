ffbbb60752e1a92027448ece9892460d9d520574
marco <marcoogarbini@gmail.com>
2024-09-03 17:52:16 +0200

    teacher profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  def showe
+  def show
+    @user = User.find(params[:id])
     @challenges = Challenge.where(level: current_user.knowledge_level)
   end
   
@@ -9,5 +10,15 @@ class UsersController < ApplicationController
 
   def challenges_per_day
     @full_page = true
+  end
+
+  def update_gems
+    @user = User.find(params[:id])
+    @user.update(ruby_gems: @user.ruby_gems + params[:ruby_gems].to_i)
+
+    # @user.ruby_gems += params[:user][:ruby_gems].to_i
+    # @user.save
+
+    redirect_to profile_path(@user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,15 @@ Rails.application.routes.draw do
   get '/challenges_per_day', to: "users#challenges_per_day", as: "challenges_per_day"
   resources :challenges, only: [:index, :show]
   resources :user_answers, only: [:create]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "users/:id/profile", to: "users#show", as: :profile
+  post "users/:id/update_gems", to: "users#update_gems", as: :update_gems
+
 
   # Defines the root path route ("/")
   # root "posts#index"
