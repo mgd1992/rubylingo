d46c93d274c1e1fc32efc43c4633d0039d048780
moisesvelasquez <moises.leonardo.velasquez.g@gmail.com>
2024-09-03 18:58:55 +0200

    first changes completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :user_challenges
   has_many :user_answers, through: :user_challenges
+  has_one_attached :photo
 
   after_save :set_user_challenge, if: -> { saved_change_to_knowledge_level? }
 
